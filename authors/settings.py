307d2a999c15b9d30d8b27c9b89472d2db747647
koechkevin <39160236+koechkevin@users.noreply.github.com>
2018-09-26 14:26:48 +0300

    [Chore #160720222] configure authors haven to use postgreSQL as db engine (#4)
    * [Chore #160720222] configure authors haven to use postgreSQL as db engine
    
    * [Chore #160720222] revert to manual jwt as initial
    
    * [Chore #160720222] update requirements.txt file
    
    * [Chore #160720222] resolve merge conflicts
    
    * [Chore #160720222] resolve merge conflicts
    
    * [Chore #160720222] resolve merge conflicts

--- authors/settings.py
+++ authors/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'authors.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME'),
+        'PASSWORD':os.getenv('DB_PASSWORD'),
+        'HOST':os.getenv('DB_HOST'),
+        'USER':os.getenv('DB_USER'),
+        'PORT':'5432',
     }
 }
 
