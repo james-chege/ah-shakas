50e23cc733494a90b48b8655cfdd396aa688c52f
Fawaz Farid <fawwazally@gmail.com>
2018-10-03 12:51:43 +0300

    [CHORE #160577473] Add missing django-rest-swagger lib (#13)

--- authors/settings.py
+++ authors/settings.py
@@ -86,10 +86,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.getenv('DB_NAME'),
-        'PASSWORD':os.getenv('DB_PASSWORD'),
-        'HOST':os.getenv('DB_HOST'),
-        'USER':os.getenv('DB_USER'),
-        'PORT':'5432',
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'USER': os.getenv('DB_USER'),
+        'PORT': '5432',
     }
 }
 
@@ -152,4 +152,3 @@ REST_FRAMEWORK = {
 
 # Activate django-heroku for Heroku
 django_heroku.settings(locals())
-
