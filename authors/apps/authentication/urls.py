944fcaaae438c80066935db3a32be2eeab28eeba
james-chege <jymeschege@gmail.com>
2018-10-03 09:00:38 +0300

    [FEATURE - #160577476] - Test for user verification

--- authors/apps/authentication/urls.py
+++ authors/apps/authentication/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('user/', UserRetrieveUpdateAPIView.as_view()),
     path('users/', RegistrationAPIView.as_view(), name='user-registration'),
     path('users/login/', LoginAPIView.as_view()),
-    path('verify/<str:token>', VerifyAPIView.as_view()),
+    path('verify/<str:token>', VerifyAPIView.as_view(), name='verify'),
 ]
